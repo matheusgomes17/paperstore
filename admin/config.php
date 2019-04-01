5a9e6da74290a28d74f979d25217d2bd79a77c6f
Matheus Gomes <matheus@lenzy.com.br>
2019-04-01 20:50:50 -0300

    Projeto em producao

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/fillipe/admin/');
+define('HTTP_CATALOG', 'http://localhost/fillipe/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/fillipe/admin/');
+define('HTTPS_CATALOG', 'http://localhost/fillipe/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/fillipe/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/fillipe/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/fillipe/image/');
+define('DIR_STORAGE', 'C:/xampp/htdocs/fillipe/storage/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/fillipe/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'fillipe');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
